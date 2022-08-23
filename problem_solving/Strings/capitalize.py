d8732aeefa1fddfbcf0e87c78b66be4559fedcf1
Bogdan <b.koshtura@gmail.com>
2022-08-23 13:16:36 +0300

    New solution

---
+++ problem_solving/Strings/capitalize.py
@@ -0,0 +1,23 @@
+#Problem: https://www.hackerrank.com/challenges/capitalize/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    return " ".join(word.capitalize() for word in s.split(' '))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
